1c709f3fcab848c63449004bf921c01d2e166d57
LizAharonian <lizaharonian@gmail.com>
2018-04-19 10:59:34 +0300

    fix bug incorrect param

--- main.c
+++ main.c
@@ -3,33 +3,33 @@
  * 316584960
  */
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+//define
 #define Fail -1
 #define TRUE 1
 #define FALSE 0
-#define ERROR "Error in system call"
-//define
 #define SIZE 1
 #define BULK 1
 #define SPACE 32
 #define A 65
 #define Z 90
+#define ERROR "Error in system call"
+
+//declarations
 int isEqual(char * firstFile, char * secondFile2);
-
 int isSimilar(char * firstFile, char * secondFile2);
-
 void handleFailure();
-
 char toLower(char character);
 
-int main(int argc, char** argv) {
 
+FILE *createFileStream(char *file);
+
+int main(int argc, char** argv) {
+    //validation on num of input args
     if (argc!=3) {
         handleFailure();
-
     }
-
+    //classify the docs
     if (isEqual(argv[1], argv[2])) {
         printf("3");
         return 3;
@@ -56,6 +56,7 @@ int isSimilar(char * firstFile, char * secondFile2) {
     }
     FILE *file2 = fopen(secondFile2, "rb");
     if (file2 == NULL) {
+        fclose(file1);
         handleFailure();
     }
     char buffer1[SIZE];
@@ -76,23 +77,23 @@ int isSimilar(char * firstFile, char * secondFile2) {
         }
         //compare current chars
         if (*buffer1 != *buffer2) {
+            //while loop places buffer1 on char which differ from space or '\n'
             while ((*buffer1 == SPACE || *buffer1 == '\n') &&
                    (status = fread(buffer1, sizeof(buffer1), BULK, file1)) != 0) {
                 if (status == Fail) {
+                    fclose(file1);
+                    fclose(file2);
                     handleFailure();
+
                 }
 
             }
-            //lowerCase both chars
-            *buffer1 = toLower(*buffer1);
-            *buffer2 = toLower(*buffer2);
-            if (*buffer1 != *buffer2) {
-                return FALSE;
-            }
-
+            //while loop places buffer2 on char which differ from space or '\n'
             while ((*buffer2 == SPACE || *buffer2 == '\n') &&
                    (status = fread(buffer2, sizeof(buffer2), BULK, file2)) != 0) {
                 if (status == Fail) {
+                    fclose(file1);
+                    fclose(file2);
                     handleFailure();
                 }
 
@@ -100,12 +101,17 @@ int isSimilar(char * firstFile, char * secondFile2) {
             //lowerCase both chars
             *buffer1 = toLower(*buffer1);
             *buffer2 = toLower(*buffer2);
+            //make the comparison again
             if (*buffer1 != *buffer2) {
+                fclose(file1);
+                fclose(file2);
                 return FALSE;
             }
         }
 
     }
+    fclose(file1);
+    fclose(file2);
     return TRUE;
 }
 
@@ -120,12 +126,16 @@ char toLower(char character) {
 
 int isEqual(char * firstFile, char * secondFile2) {
     //open files
+   // FILE* file1 = createFileStream(firstFile);
     FILE* file1 = fopen(firstFile, "rb");
     if (file1 ==NULL) {
         handleFailure();
     }
     FILE* file2 = fopen(secondFile2, "rb");
     if (file2 ==NULL) {
+
+        fclose(file1);
+
         handleFailure();
     }
     char buffer1[SIZE];
@@ -135,14 +145,27 @@ int isEqual(char * firstFile, char * secondFile2) {
     while (((status = fread(buffer1, sizeof(buffer1), BULK, file1))!=0 )&&
             ((status =fread(buffer2, sizeof(buffer2), BULK, file2))!=0)) {
         if (status ==Fail) {
+            fclose(file1);
+            fclose(file2);
             handleFailure();
         }
         //compare current chars
-        if (!(*buffer1==*buffer2)) {
+        if (*buffer1!=*buffer2) {
+            fclose(file1);
+            fclose(file2);
             return FALSE;
         }
 
     }
+    fclose(file1);
+    fclose(file2);
     return TRUE;
 }
+
+/*FILE *createFileStream(char *file) {
+    FILE* fileStream = fopen(file, "rb");
+    if (fileStream ==NULL) {
+        handleFailure();
+    }
+}*/
 
